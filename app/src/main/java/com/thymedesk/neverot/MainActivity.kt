8c4ab40ed92f17ee220f880b35fbabfe1a421e6a
Gyeongrok Kim <gyeongrok.kim@gomsang.com>
2023-01-02 22:12:50 +0900

    : project initialized

---
+++ app/src/main/java/com/thymedesk/neverot/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.thymedesk.neverot
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.thymedesk.neverot.ui.theme.NeverotTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NeverotTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NeverotTheme {
+        Greeting("Android")
+    }
+}
